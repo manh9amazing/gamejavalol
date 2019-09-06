b1dea5c99bd73c0c66226e66c16a4341b8088a93
manh9amazing <phumanh2003@gmail.com>
2019-09-06 20:53:15 +0700

    update: load background image

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -1,13 +1,31 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.font.ImageGraphicAttribute;
+import java.io.File;
+import java.io.IOException;
 
 public class GameCanvas extends JPanel {
+    Image background;
+    Image char1;
+    public GameCanvas(){
+        try {
+            this.background = ImageIO.read(new File("assets/images/background/0.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //IO= in and out
+        //copy relative path
+        // vi neu dung copy path thi se bat dau tu o D cua may,neu cho nguoi khac choi game thi ko choi dc
+    }
     @Override
     protected void paintComponent(Graphics g) {
         //graphic g nhu 1 cai but ve, dung method cua g de ve
 //        g.drawRect(0,0,100,200);
         g.setColor(Color.black);
         g.fillRect(0,0,100,200);
+        g.drawImage(this.background, 0, 0, null);
 
     }
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import javax.swing.event.SwingPropertyChangeSupport;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
@@ -10,45 +11,15 @@ public class GameWindow extends JFrame {
 //        this.setVisible(true);
         this.setResizable(false);
         //khong the nhan thu nho, tranh vo hinh anh
-        this.setSize(600,600);
+        this.setSize(384,600);
 
         this.setContentPane(this.canvas);
         this.setVisible(true);
 
-        this.addWindowListener(new WindowListener() {
+        this.addWindowListener(new WindowAdapter() {
             @Override
-            public void windowOpened(WindowEvent windowEvent) {
-
-            }
-
-            @Override
-            public void windowClosing(WindowEvent windowEvent) {
+            public void windowClosing(WindowEvent e) {
                 System.exit(0);
-                System.out.println("window closing");
-            }
-
-            @Override
-            public void windowClosed(WindowEvent windowEvent) {
-            }
-
-            @Override
-            public void windowIconified(WindowEvent windowEvent) {
-
-            }
-
-            @Override
-            public void windowDeiconified(WindowEvent windowEvent) {
-
-            }
-
-            @Override
-            public void windowActivated(WindowEvent windowEvent) {
-
-            }
-
-            @Override
-            public void windowDeactivated(WindowEvent windowEvent) {
-
             }
         });
     }
