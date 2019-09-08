aa75838877ba9aa486a00ae1e83d4bd151ffac25
manh9amazing <phumanh2003@gmail.com>
2019-09-08 11:39:33 +0700

    update: add blue and pink enemies (can flap wings)

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -8,27 +8,43 @@ import java.awt.font.ImageGraphicAttribute;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 public class GameCanvas extends JPanel {
 
     Image background;
+    Image blueState;
+    Image pinkState;
+
+    Image blueState0;
+    Image blueState1;
+    Image blueState2;
+    Image blueState3;
+
+    Image pinkState0;
+    Image pinkState1;
+    Image pinkState2;
+    Image pinkState3;
 
     Image player;
+
     int PlayerX;
     int PlayerY;
 
     int enemyBlackX;
     int enemyBlackY;
 
-    int enemyBlueX;
-    int enemy1BLueY;
+    int enemyBlueX = 0;
+    int enemyBlueY = 0;
 
-    int enemyPinkX;
-    int enemyPinkY;
+    int enemyPinkX = 340;
+    int enemyPinkY= 0 ;
 
     public int scroll;
     int Y_posi;
     boolean end = false;
+
+    int cntState = 0;
 
 //    public BufferedImage buf_img;
     public GameCanvas(){
@@ -38,6 +54,17 @@ public class GameCanvas extends JPanel {
 //            this.buf_img = ImageIO.read(new File("assets/images/background/0.png"));
 //            this.buf_img = buf_img.getSubimage(0,0 ,384 ,600);
             this.player = ImageIO.read(new File("assets/images/players/straight/0.png"));
+
+            this.blueState0 = ImageIO.read(new File("assets/images/enemies/level0/blue/0.png"));
+            this.blueState1 = ImageIO.read(new File("assets/images/enemies/level0/blue/1.png"));
+            this.blueState2 = ImageIO.read(new File("assets/images/enemies/level0/blue/2.png"));
+            this.blueState3 = ImageIO.read(new File("assets/images/enemies/level0/blue/3.png"));
+
+            this.pinkState0 = ImageIO.read(new File("assets/images/enemies/level0/pink/0.png"));
+            this.pinkState1 = ImageIO.read(new File("assets/images/enemies/level0/pink/1.png"));
+            this.pinkState2 = ImageIO.read(new File("assets/images/enemies/level0/pink/2.png"));
+            this.pinkState3 = ImageIO.read(new File("assets/images/enemies/level0/pink/3.png"));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -98,13 +125,21 @@ public class GameCanvas extends JPanel {
         g.setColor(Color.black);
 //        g.fillRect(0,0,100,200);
         Y_posi = -2509 + scroll;
-        this.oneRun();
+        this.infiniteRun();
         if (end == false) {
             g.drawImage(this.background, 0, Y_posi, null);
         }
         //truc x roi y
-        g.drawImage(this.player, this.PlayerX, this.PlayerY, null);
+        g.drawImage(this.player, PlayerX, PlayerY, null);
+
+        this.enemyBlueRun();
+        g.drawImage(blueState, enemyBlueX, enemyBlueY, null);
+
+        this.enemyPinkRun();
+        g.drawImage(pinkState, enemyPinkX, enemyPinkY, null);
+
     }
+
     public void oneRun(){
         if (Y_posi>0){
             end = true;
@@ -115,11 +150,130 @@ public class GameCanvas extends JPanel {
             scroll = 0;
         }
     }
+
     public  void  run() {
-        System.out.println(scroll);
+//        System.out.println(scroll);
         scroll+=10;
+        cntState+=1;
+        this.stateClamp();
+        System.out.println(cntState);
     }
+
+    public void stateClamp(){
+        if (cntState>15){
+            cntState = 0;
+        }
+    }
+
     public void enemyBlueRun(){
+        this.enemyBlueState();
+        this.enemyBlueXY();
+    }
+    public void enemyBlueClamp(){
+        if (enemyBlueX <0){
+            enemyBlueX = 0;
+        }
+        if (enemyBlueX >340){
+            enemyBlueX = 340;
+        }
+        if (enemyBlueY <0){
+            enemyBlueY = 0;
+        }
+        if (enemyBlueY >530 ){
+            enemyBlueY = 530;
+        }
+    }
+    public void enemyBlueXY(){
+        Random rand2 = new Random();
+        int choiceXY = rand2.nextInt(2);
+        int choiceDI = rand2.nextInt(2);
+        if (choiceXY==0){
+            if(choiceDI==0){
+                enemyBlueX-=5;
+            }
+            if(choiceDI==1){
+                enemyBlueX+=5;
+            }
+        }
+        if (choiceXY==1){
+            if(choiceDI==0){
+                enemyBlueY-=5;
+            }
+            if(choiceDI==1){
+                enemyBlueY+=5;
+            }
+        }
+        this.enemyBlueClamp();
+    }
+    public void enemyBlueState(){
+        if (cntState ==0){
+            blueState = blueState0;
+        }
+        if (cntState ==5){
+            blueState = blueState1;
+        }
+        if (cntState ==10){
+            blueState = blueState2;
+        }
+        if (cntState ==15){
+            blueState = blueState3;
+        }
 
     }
+
+    public void enemyPinkRun(){
+        this.enemyPinkState();
+        this.enemyPinkXY();
+    }
+    public void enemyPinkClamp(){
+        if (enemyPinkX <0){
+            enemyPinkX = 0;
+        }
+        if (enemyPinkX >340){
+            enemyPinkX = 340;
+        }
+        if (enemyPinkY <0){
+            enemyPinkY = 0;
+        }
+        if (enemyPinkY >530 ){
+            enemyPinkY = 530;
+        }
+    }
+    public void enemyPinkXY(){
+        Random rand2 = new Random();
+        int choiceXY = rand2.nextInt(2);
+        int choiceDI = rand2.nextInt(2);
+        if (choiceXY==0){
+            if(choiceDI==0){
+                enemyPinkX-=5;
+            }
+            if(choiceDI==1){
+                enemyPinkX+=5;
+            }
+        }
+        if (choiceXY==1){
+            if(choiceDI==0){
+                enemyPinkY-=5;
+            }
+            if(choiceDI==1){
+                enemyPinkY+=5;
+            }
+        }
+        this.enemyPinkClamp();
+    }
+    public void enemyPinkState(){
+        if (cntState ==0){
+            pinkState = pinkState0;
+        }
+        if (cntState ==5){
+            pinkState = pinkState1;
+        }
+        if (cntState ==10){
+            pinkState = pinkState2;
+        }
+        if (cntState ==15){
+            pinkState = pinkState3;
+        }
+    }
+
 }
